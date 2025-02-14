30260f887bfeabcc163c019f0256dac1eff31a27
Shmuel Levinson <levinson.shmuel@gmail.com>
2025-02-15 00:41:09 +0200

    vite.config.ts fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Ensures modern JS output
+    modulePreload: { polyfill: false }, // Avoids unnecessary preloading issues
+  }
 })
